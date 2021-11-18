a72b9d9628c1c9a34d0d3ba8782bcd022b21881e
Linamcaro <lina.mcaro@gmail.com>
2021-11-18 16:02:54 -0500

    "create the layout for the POI list"

---
+++ app/src/main/java/com/linamcaro/fastandroid/POI_list_Activity.kt
@@ -0,0 +1,11 @@
+package com.linamcaro.fastandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class POI_list_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poi_list)
+    }
+}
